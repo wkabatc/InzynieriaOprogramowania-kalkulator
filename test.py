1e96042119323244174a0b49e2f1b016a6b35ae7
Wojciech Kabatc <wojciechkabatc@gmail.com>
2022-03-27 17:35:16 +0200

    poprawiono funkcję

--- test.py
+++ test.py
@@ -2,13 +2,14 @@ def hello(name):
     return "Hello" + str(name)
 
 def odejmij(a,b):
-    return a-b
+    wynik = float(a) - float(b)
+    return wynik
 
 def dodaj(a,b):
     wynik = float(a) + float(b)
     return wynik
 
-pierwsza = input()
-druga = input()
+pierwsza = int(input())
+druga = int(input())
 
 print (dodaj(pierwsza, druga))
