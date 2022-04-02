4a6affc9abaaf5c48b492061c72f70e4c4f35f8c
FrankSpinachi <76732249+FrankSpinachi@users.noreply.github.com>
2022-04-02 17:19:23 +0200

    dzielenie
    Dodanie dzielenia do kalulkatora

--- test.py
+++ test.py
@@ -13,6 +13,12 @@ def pomnoz(a,b):
     wynik = float(a) * float(b)
     return wynik
 
+def dzielenie(x,y):
+    try:
+        return x/y
+    except ZeroDivisionError:
+        return 0
+
 pierwsza = input()
 druga = input()
 
